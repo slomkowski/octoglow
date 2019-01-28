44016dc77021a175b0e3e7267d8c2345347c51ed
Michał Słomkowski <m.slomkowski@gmail.com>
2019-01-28 19:34:22 +0100

    Implement ranges and baseline test for brightness.

--- software/octoglowd2/src/main/kotlin/eu/slomkowski/octoglow/octoglowd/BrightnessController.kt
+++ software/octoglowd2/src/main/kotlin/eu/slomkowski/octoglow/octoglowd/BrightnessController.kt
@@ -2,7 +2,10 @@ package eu.slomkowski.octoglow.octoglowd
 
 import mu.KLogging
 import org.shredzone.commons.suncalc.SunTimes
-import java.time.*
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
 import java.util.*
 
 /**
@@ -19,13 +22,29 @@ class BrightnessController {
         TRANSITION_TO_MIDDAY
     }
 
-    companion object :KLogging() {
+    class PodRange(val pod: PartOfTheDay, val start: Duration, end: Duration) {
 
-        private val transitionDuration : Duration = Duration.ofHours(1)
+        val end: Duration = when (end < start) {
+            true -> end.plusDays(1)
+            else -> end
+        }
 
-        private fun Date.toLocalDateTime() : LocalDateTime =toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+        init {
+            require(!this.start.isNegative)
+            require(!this.end.isNegative)
+            require(this.start < this.end)
+        }
 
-        fun calculateBrightnessFraction(ts: LocalDateTime, goToSleep : LocalTime):PartOfTheDay  {
+        fun surrounds(d: Duration): Boolean = d in start..end
+    }
+
+    companion object : KLogging() {
+
+        private val transitionDuration: Duration = Duration.ofHours(1)
+
+        private fun Date.toLocalDateTime(): LocalDateTime = toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+
+        fun calculateBrightnessFraction(ts: LocalDateTime, goToSleep: LocalTime): PartOfTheDay {
 
             val sunTimes = SunTimes.compute()
                     .at(52.395869, 16.929220)
@@ -33,19 +52,42 @@ class BrightnessController {
                     .oneDay()
                     .execute()
 
-            val sunrise = checkNotNull(sunTimes.rise?.toLocalDateTime()) {"cannot calculate sunrise for $ts."}
-            val sunset = checkNotNull(sunTimes.set?.toLocalDateTime()) {"cannot calculate sunset for $ts."}
-            check(sunset.isAfter(sunrise))
+            val (sunrise, sunset) = listOf(sunTimes.rise, sunTimes.set).map {
+                checkNotNull(it?.toLocalDateTime()) { "cannot calculate sunrise/sunset for $ts." }.toLocalTime()
+            }
+            check(sunset > sunrise)
+            logger.debug { "On ${ts.toLocalDate()} sun is up from $sunrise to $sunset." }
 
-            logger.debug { "Times: $sunrise $sunset." }
+            val ranges = calculateRanges(sunrise, sunset, goToSleep, transitionDuration)
 
-            val halfTransition = transitionDuration.dividedBy(2)
+            val t = Duration.between(LocalTime.MIN, ts.toLocalTime())
 
+            val currentRange = checkNotNull(ranges.firstOrNull { it.surrounds(t) || it.surrounds(t.plusDays(1)) }) { "range not found for $ts" }
 
+            return currentRange.pod
+        }
 
+        fun calculateRanges(sunrise: LocalTime, sunset: LocalTime, goToSleep: LocalTime, transition: Duration): List<PodRange> {
+            require(sunset.isAfter(sunrise))
+            require(transition < Duration.ofHours(3))
 
+            val sr = Duration.between(LocalTime.MIN, sunrise)
+            val ss = Duration.between(LocalTime.MIN, sunset)
+            val htt = transition.dividedBy(2)
 
+            //val isSleepAfterMidnight =
+            val sleepTime = Duration.between(LocalTime.MIN, goToSleep).plus(when (goToSleep.isBefore(LocalTime.NOON)) {
+                true -> Duration.ofDays(1)
+                false -> Duration.ZERO
+            })
 
+            return listOf(
+                    PodRange(PartOfTheDay.MIDDAY, sr + htt, ss - htt),
+                    PodRange(PartOfTheDay.TRANSITION_TO_EVENING, ss - htt, ss + htt),
+                    PodRange(PartOfTheDay.EVENING, ss + htt, sleepTime - htt),
+                    PodRange(PartOfTheDay.TRANSITION_TO_SLEEPING_TIME, sleepTime - htt, sleepTime + htt),
+                    PodRange(PartOfTheDay.SLEEPING_TIME, sleepTime + htt, sr - htt),
+                    PodRange(PartOfTheDay.TRANSITION_TO_MIDDAY, sr - htt, sr + htt))
         }
     }
 }
--- software/octoglowd2/src/test/kotlin/eu/slomkowski/octoglow/octoglowd/BrightnessControllerTest.kt
+++ software/octoglowd2/src/test/kotlin/eu/slomkowski/octoglow/octoglowd/BrightnessControllerTest.kt
@@ -1,14 +1,34 @@
 package eu.slomkowski.octoglow.octoglowd
 
-import org.junit.jupiter.api.Assertions.*
+import eu.slomkowski.octoglow.octoglowd.BrightnessController.Companion.calculateBrightnessFraction
+import eu.slomkowski.octoglow.octoglowd.BrightnessController.Companion.calculateRanges
+import mu.KLogging
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
+import java.time.Duration
 import java.time.LocalDateTime
 import java.time.LocalTime
+import kotlin.test.assertTrue
 
 class BrightnessControllerTest {
-  @Test
-  fun testCalculateBrightnessFraction() {
-  BrightnessController.calculateBrightnessFraction(LocalDateTime.of(2019, 1, 27, 12, 34), LocalTime.of(23, 16))
-  }
- }
+    companion object : KLogging()
+
+    @Test
+    fun testCalculateBrightnessFraction() {
+        val r = calculateBrightnessFraction(LocalDateTime.of(2019, 1, 28, 1, 50), LocalTime.of(23, 16))
+        logger.debug { "Found $r." }
+        assertNotNull(r)
+    }
+
+    @Test
+    fun testCalculateRanges() {
+        testForEveryHour(calculateRanges(LocalTime.of(7, 32), LocalTime.of(17, 23), LocalTime.of(23, 3), Duration.ofHours(1)))
+    }
+
+    private fun testForEveryHour(ranges: List<BrightnessController.PodRange>) {
+        (0 until 24 * 60 * 60).map { LocalTime.ofSecondOfDay(it.toLong()) }.forEach {
+            val t = Duration.between(LocalTime.MIN, it)
+            assertTrue { ranges.any { r -> r.surrounds(t) || r.surrounds(t.plusDays(1)) } }
+        }
+    }
+}
