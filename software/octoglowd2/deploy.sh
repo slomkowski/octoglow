7c2a2d98d5af6be91e99a2f2be7ec38399ed775d
Michał Słomkowski <m.slomkowski@gmail.com>
2019-03-13 01:27:30 +0100

    Add google concurrent to ignore list.

--- software/octoglowd2/deploy.sh
+++ software/octoglowd2/deploy.sh
@@ -3,7 +3,7 @@
 mvn -DskipTests package
 
 OCTOGLOW_HOST='octoglow'
-JAR_FILE='target/octoglowd-1.0-SNAPSHOT-jar-with-dependencies.jar'
+JAR_FILE='target/octoglowd-1.0-SNAPSHOT-jar-with-dependencies-with-exclude-files.jar'
 
-scp ${JAR_FILE} ${OCTOGLOW_HOST}:/home/octoglow/octoglowd/
+scp ${JAR_FILE} ${OCTOGLOW_HOST}:/home/octoglow/octoglowd/octoglowd.jar
 ssh ${OCTOGLOW_HOST} 'supervisorctl restart octoglowd'
