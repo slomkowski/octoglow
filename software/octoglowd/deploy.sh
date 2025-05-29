5c226331ce1938ec03594410806d5df84708260e
Michał Słomkowski <m.slomkowski@gmail.com>
2025-05-29 10:06:49 +0200

    Clean up Gradle.

--- software/octoglowd/deploy.sh
+++ software/octoglowd/deploy.sh
@@ -3,10 +3,10 @@
 set -e
 set -u
 
-JAVA_HOME=/usr/lib/jvm/java-17-openjdk/ mvn -DskipTests package
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk/ ./gradlew shadowJar
 
 OCTOGLOW_HOST='octoglow'
-JAR_FILE='target/octoglowd-1.0-SNAPSHOT-jar-with-dependencies-with-exclude-files.jar'
+JAR_FILE='build/libs/octoglowd-all.jar'
 
 scp ${JAR_FILE} ${OCTOGLOW_HOST}:/home/octoglow/octoglowd/octoglowd.jar
 ssh ${OCTOGLOW_HOST} 'supervisorctl restart octoglowd'
